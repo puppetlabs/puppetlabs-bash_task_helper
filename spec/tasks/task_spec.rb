70037258ab367ea6e3b1bf4b4b5b543c23644fa1
Adrian <sensei.loafage@gmail.com>
2021-11-05 10:31:58 -0700

    Add spec test

---
+++ spec/tasks/task_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+require 'json'
+
+describe 'bash_task_helper' do
+  context 'when using the helper script' do
+    it {
+      # Tasks expect certain PT_ environment variables to be set
+      # We only need _installdir, so set it one directory up from the module root
+      ENV['PT__installdir'] = File.join(File.dirname(__FILE__), '../../../')
+
+      # Get the path to the task based on the absolute path of this file
+      task = File.join(File.dirname(__FILE__), '../../examples/mytask.sh')
+      out = %x[#{task}]
+      # Success for this task is essentially determined by if we get a valid JSON object back
+      expect(['error', 'success'].include? out['status'])
+    }
+  end
+end
