cb48339e28ef5d0e76b8e3ff6c6cfb4d38a957e5
Sean Millichamp <sean@bruenor.org>
2023-09-09 20:55:26 +0000

    Fix task-output-json to work as documented
    Currently the task_output_keys/values are always printed with
    double quotes around the value, preventing task-output-json
    from working as intended.
    
    Additionally, this includes a refactoring of the unit test as
    the current one is flawed and does not test anything meaningful.

--- examples/mytask.sh
+++ examples/mytask.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
-# shellcheck disable=SC1090
+# shellcheck disable=SC1090,SC2154
 # This is disabled in CI, but we'll disable it here as well
 declare PT__installdir
 source "$PT__installdir/bash_task_helper/files/task_helper.sh"
 
-if puppet resource service puppet | grep -q stopped; then
-    #Start the Puppet service
-     puppet resource service puppet ensure=running || task-fail "Couldn't start puppet service"
-else
-  task-fail "Puppet is already running or not installed"
+if [ "$run_type" == 'fail' ]; then
+  task-fail "This task failed"
+elif [ "$run_type" == 'pass' ]; then
+  task-succeed "This task succeeded"
+elif [ "$run_type" == 'output' ]; then
+  task-output "string1" "abcd"
+  task-output "string-numeric" 42
+  task-output-json "string2" '"abcd"'
+  task-output-json "number" 42
+  task-output-json "bool" true
+  task-output "complex-string" "This is a \"complex string\".\n\tSecond line."
 fi
 
 task-succeed
